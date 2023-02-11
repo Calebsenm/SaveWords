b0be5b9aab75b8915e3501d47d5f5be524a92f2d
calebsenm <you@gmail.com>
2023-02-10 23:30:19 -0500

    Firs step

---
+++ main.go
@@ -0,0 +1,43 @@
+
+package main 
+
+
+import "fmt"
+
+func main(){
+
+  // this is for opcions an Login 
+  Options := map[int] string{
+    1 : "Login"    ,
+    2 : "Sign In"  ,
+    3 : "Options " ,
+  } 
+
+  // Print the opcions 
+  fmt.Println("Welcome ! Plese enter Login ")
+  
+  for index,value := range Options{
+    fmt.Println(" ",index , " ", value)
+  }
+
+  // ask for a Option 
+  var choise int 
+  fmt.Print("  > ")
+  fmt.Scan(&choise)
+
+  
+  if choise == 1{
+     fmt.Println("Your choise ", Options[choise])
+ 
+  } else if choise == 2{
+      fmt.Println("Your choise ", Options[choise])
+
+  } else if  choise == 3 {
+      fmt.Println("Your choise ", Options[choise])
+ 
+  } else {
+    fmt.Println("Error")
+  }
+
+
+}
